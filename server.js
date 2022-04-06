c6814ec0ccb0cb553c4d821b26ee5416bffacec6
Seungwoo Lee <alfred@hpcnt.com>
2022-04-07 01:12:55 +0900

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const io = require("socket.io")(server, {
         origin: '*'
     }
 });
-
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -33,9 +32,6 @@ io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId, userName) => {
         socket.join(roomId);
         socket.to(roomId).broadcast.emit("user-connected", userId);
-        socket.on("message", (message) => {
-            io.to(roomId).emit("createMessage", message, userName);
-        });
     });
 });
 
